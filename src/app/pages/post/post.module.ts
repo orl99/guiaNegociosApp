dbeeb83b8235d21b6b222555cd97a5534f442ab5
Orlando Garcia <orlando.garcia.64@hotmail.com>
2020-05-13 13:17:08 -0500

    app in beta version

--- src/app/pages/post/post.module.ts
+++ src/app/pages/post/post.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { PostPageRoutingModule } from './post-routing.module';
 
 import { PostPage } from './post.page';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    PostPageRoutingModule
+    PostPageRoutingModule,
+    SharedModule
   ],
   declarations: [PostPage]
 })
